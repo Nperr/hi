d08c812174f84735ed2e466599a8b90ae7c0e299
tang-crush-main <leotranngunguoi128@gmail.com>
2021-08-15 09:00:20 +0700

    Tang-cr

---
+++ js/config 1.jsx
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hello chị iu !',
+    introDesc: `Em có điều này muốn hỏi chị, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Chơii',
+    title: 'Chị yêu em có phải không ?',
+    desc: 'Chị không trả lời hoặc thoát ra, tức là chị là người yêu em đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'em bị ảo à :333',
+    question:'Trên thế giới có 7 tỉ người mà sao chị lại yêu em <3',
+    btnReply: 'Gửi cho em đi <3',
+    reply: 'Tại vì em cutee hihi <33333333',
+    mess: 'Em biết mà !. Love you 300.000 😘😘',
+    messDesc: 'Còn giờ thì chờ gì nữa mà không inbox cho em đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100064206967687' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
